9b23c30cb6111a7f8e4b4c6db8e790da47221ba7
Alexin-CH <alexin.choisnet@gmail.com>
2026-02-12 21:59:13 +0100

    Loop through templates

--- src/main.py
+++ src/main.py
@@ -9,17 +9,25 @@ try_cuda = True
 device = torch.device('cuda' if try_cuda and torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
 
-torch.autograd.set_detect_anomaly(True)
-
 if __name__ == "__main__":
-    trained_model, raytracer, losses = train_surface(
-        epochs=2e2,
-        lr=1e-4,
-        device=device,
-        gif=20
-    )
 
-    # Plot losses
+    for target in ["square", "bat", "cards", "heart", "pi", "plus", "qm", "spiral"]:
+        try:
+            trained_model, raytracer, losses = train_surface(
+                target=target,
+                res_divfactor=1,
+                epochs=2e2,
+                batch_size=2048*2,
+                lr=1e-4,
+                device=device,
+                gif=20
+            )
+        except Exception as e:
+            print(target)
+            print(e)
+            print()
+
+    # # Plot losses
     # losses = torch.tensor(losses)
     # plt.figure()
     # plt.plot(losses[:, 2])
@@ -30,7 +38,7 @@ if __name__ == "__main__":
     # plt.grid()
     # plt.pause(2)
 
-    # Display results
+    # # Display results
     # source = sample_beam(50*1000, 1).to(device)
     # plot_results_scene(
     #     model=trained_model,
