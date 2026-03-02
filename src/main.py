2d21af568e182fedce5f28d902419bf43f788a85
Alexin-CH <alexin.choisnet@gmail.com>
2026-03-02 14:39:01 +0100

    Add total running time indicator

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 import torch
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
@@ -11,14 +12,15 @@ print(f"Using device: {device}")
 print(f"Seed: {torch.seed()}")
 
 if __name__ == "__main__":
-
+    
+    t0 = time.time()
     for target in ["pi", "spiral", "square", "bat", "cards", "heart", "plus", "qm"]:
         trained_model, raytracer, losses = train_surface(
             target=target,
-            res_factor=10,
-            epochs=100,
+            res_factor=2,
+            epochs=2e2,
             num_batch=1,
-            batch_size=2048*2,
+            batch_size=2500,
             lr=1e-5,
             device=device,
             gif=40
@@ -48,3 +50,14 @@ if __name__ == "__main__":
 
         plt.savefig(f"target_{target}_loss.png", bbox_inches='tight', pad_inches=0.1, dpi=100, facecolor="white")
         plt.close()
+
+        break
+
+    t_final = time.time() - t0
+    t_h = t_final // 3600
+    t_m = (t_final % 3600) // 60
+    t_s = t_final % 60
+
+    print()
+    print(f"Elapsed Time: {int(t_h)} hours, {int(t_m)} minutes, {int(t_s)} seconds")
+    print()
